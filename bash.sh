6510eca8ed8e03666f1ee160280af2cbf05b18ae
sunshizhu <sshizhu@gmail.com>
2016-08-24 15:57:07 -0700

    bash upload testign

---
+++ bash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo " The script starts now!"
+echo
+echo "Hi $USER! "
+
+echo "I am setting two vars now."
+COLOR="black"
+VALUE="9"
+
+echo "this is a string $COLOR"
+echo "This is a value: $VALUE"
+echo
+
+echo
